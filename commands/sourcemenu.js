e7137fb6c038123c94c479c4dea3a2677865c342
Nguyen Minh Kha <cskhchungchilayngay@gmail.com>
2026-07-08 20:48:43 +0700

    Create sourcemenu.js

---
+++ commands/sourcemenu.js
@@ -0,0 +1,22 @@
+module.exports = async (ctx) => {
+    await ctx.reply(`
+🌐━━━━━━━━━━━━━━━━━━━━🌐
+       WEBSITE TOOLS
+🌐━━━━━━━━━━━━━━━━━━━━🌐
+
+📄 /info <url>
+➜ Xem thông tin website
+
+📑 /html <url>
+➜ Lấy mã nguồn HTML
+
+🎨 /css <url>
+➜ Danh sách CSS
+
+⚙️ /js <url>
+➜ Danh sách JavaScript
+
+━━━━━━━━━━━━━━━━━━━━
+💻 BenDev Team
+`);
+};
