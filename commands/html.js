52b5b9aac242877b38b2b0401a5fc73e38d37f1e
Nguyen Minh Kha <cskhchungchilayngay@gmail.com>
2026-07-08 20:11:36 +0700

    Update html.js

--- commands/html.js
+++ commands/html.js
@@ -1,23 +1,29 @@
+const fs = require("fs");
+const path = require("path");
 const axios = require("axios");
 
 module.exports = async (ctx) => {
-    const args = ctx.message.text.split(" ");
+    const url = ctx.message.text.split(" ")[1];
 
-    if (args.length < 2) {
+    if (!url) {
         return ctx.reply("Ví dụ:\n/html https://example.com");
     }
-
-    const url = args[1];
 
     try {
         const res = await axios.get(url);
 
+        const file = path.join(__dirname, "source.html");
+
+        fs.writeFileSync(file, res.data);
+
         await ctx.replyWithDocument({
-            source: Buffer.from(res.data),
-            filename: "source.html"
+            source: file
         });
 
-    } catch (err) {
-        await ctx.reply("❌ Không lấy được HTML của website.");
+        fs.unlinkSync(file);
+
+    } catch (e) {
+        console.log(e);
+        ctx.reply("Không lấy được HTML.");
     }
 };
