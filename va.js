6439ccc7333dbaca89e1fdb667c1b9ccab27033d
Nguyen Minh Kha <cskhchungchilayngay@gmail.com>
2026-07-09 19:28:10 +0700

    Update va.js

--- va.js
+++ va.js
@@ -9,6 +9,7 @@ const sourceMenu = require("./commands/sourcemenu");
 const networkMenu = require("./commands/menu");
 const dnsCommand = require("./commands/dns");
 const headersCommand = require("./commands/headers");
+const redirectCommand = require("./commands/redirect");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const CHAT_ID = -1004359631890;
@@ -1020,6 +1021,7 @@ bot.command("src", sourceMenu);
 bot.command("net", networkMenu);
 bot.command("dns", dnsCommand);
 bot.command("headers", headersCommand);
+bot.command("redirect", redirectCommand);
 
 bot.command("test", (ctx) => {
     ctx.reply("OK");
