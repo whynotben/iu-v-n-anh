772885194fb0a631fd1df8163cad2361afc6fe1e
Nguyen Minh Kha <cskhchungchilayngay@gmail.com>
2026-07-23 06:35:07 +0700

    Update va.js

--- va.js
+++ va.js
@@ -34,6 +34,7 @@ const lagffCommand = require("./commands/lagff");
 const udidCommand = require("./commands/udid");
 const udidStatus = require("./commands/udidstatus");
 const createUdid = require("./commands/createudid");
+const layma = require("./commands/layma");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -1128,6 +1129,7 @@ bot.command("lagff", lagffCommand);
 bot.command("udid", udidCommand);
 bot.command("udidstatus", udidStatus);
 bot.command("createudid", createUdid);
+bot.command("layma", layma);
 
 bot.command("test", (ctx) => {
     ctx.reply("OK");
